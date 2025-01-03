9b53257cbb3b86f77dabdd64c4febeb8a2e4c6a9
Fantajī × Neko <ovosakura@gmail.com>
2025-01-03 15:17:56 +0800

    流量记录显示近35天
    流量记录显示近35天

--- app/Http/Controllers/V1/User/StatController.php
+++ app/Http/Controllers/V1/User/StatController.php
@@ -19,7 +19,7 @@ class StatController extends Controller
             'server_rate'
         ])
             ->where('user_id', $request->user['id'])
-            ->where('record_at', '>=', strtotime(date('Y-m-1')))
+            ->where('record_at', '>=', now()->subDays(35)->timestamp)
             ->orderBy('record_at', 'DESC');
         return response([
             'data' => $builder->get()
